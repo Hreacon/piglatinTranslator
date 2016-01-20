e455d82142c0fa571bf419b4ba4e8d0d2fe91395
Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-01-20 08:44:59 -0800

    add spec for moveConsonants. fails.

--- spec/specs.js
+++ spec/specs.js
@@ -3,3 +3,9 @@ describe("wordTranslator", function() {
     expect(wordTranslator('average')).to.equal('averageay');
   });
 });
+
+describe("moveConsonants", function() {
+  it("moves the first consonants to the end of the word", function() {
+    expect(moveConsonants('smoking')).to.equal('okingsmay');
+  });
+});
